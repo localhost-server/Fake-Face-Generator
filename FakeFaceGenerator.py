2911809cc82db8626fc86e031ed15952d0e39877
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-08-25 16:55:33 +0530

    Add files via upload

--- FakeFaceGenerator.py
+++ FakeFaceGenerator.py
@@ -3,9 +3,14 @@ import cv2
 import numpy as np
 import asyncio as a
 
-img= a.get_event_loop().run_until_complete(get_online_person())#.close()
+loop=a.get_event_loop()
+def facegen():
+    img= loop.run_until_complete(get_online_person())#.close()
 
-imgar=np.array(bytearray(img),dtype=np.uint8)
-img=cv2.imdecode(imgar,1)
-cv2.imshow('image',img)
-cv2.waitKey(0)
+    imgar=np.array(bytearray(img),dtype=np.uint8)
+    img=cv2.imdecode(imgar,1)
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+
+facegen()
+loop.close()
