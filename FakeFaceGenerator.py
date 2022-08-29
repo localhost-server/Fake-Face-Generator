9c1f9263a9d59d06c454e652d1d1c88502a52789
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-08-29 07:58:20 +0530

    Update FakeFaceGenerator.py

--- FakeFaceGenerator.py
+++ FakeFaceGenerator.py
@@ -1,17 +1,21 @@
 from thispersondoesnotexist import get_online_person
-import cv2 
-import numpy as np
+from  PIL import Image
 import asyncio as a
+import io
+import base64
 
 a.set_event_loop_policy(a.WindowsSelectorEventLoopPolicy())#get_event_loop()
 loop=a.new_event_loop()
-def facegen():
-    img= loop.run_until_complete(get_online_person())#.close()
+bytedata = loop.run_until_complete(get_online_person())
 
-    imgar=np.array(bytearray(img),dtype=np.uint8)
-    img=cv2.imdecode(imgar,1)
-    cv2.imshow('image',img)
-    cv2.waitKey(0)
+img=Image.open(io.BytesIO(bytedata))
+img.show()
+# def facegen():
+#     img= loop.run_until_complete(get_online_person())#.close()
 
-facegen()
+#     img=Image.frombytes(img)
+#     img=Image.imshow(img)
+    
+
+# facegen()
 loop.close()
