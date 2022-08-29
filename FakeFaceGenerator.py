8a306f8ea2211695b33b04989696565f42397964
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-08-29 08:11:57 +0530

    Update FakeFaceGenerator.py

--- FakeFaceGenerator.py
+++ FakeFaceGenerator.py
@@ -6,10 +6,12 @@ import base64
 
 a.set_event_loop_policy(a.WindowsSelectorEventLoopPolicy())#get_event_loop()
 loop=a.new_event_loop()
-bytedata = loop.run_until_complete(get_online_person())
 
-img=Image.open(io.BytesIO(bytedata))
-img.show()
+def Make_new_Face():
+    bytedata = loop.run_until_complete(get_online_person())
+
+    img=Image.open(io.BytesIO(bytedata))
+    return img.show()
 # def facegen():
 #     img= loop.run_until_complete(get_online_person())#.close()
 
