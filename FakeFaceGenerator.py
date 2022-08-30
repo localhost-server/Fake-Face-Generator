ec58ea73585f1ca81f986862af14f0f5401f3c74
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-08-30 11:09:12 +0530

    Update FakeFaceGenerator.py

--- FakeFaceGenerator.py
+++ FakeFaceGenerator.py
@@ -5,7 +5,7 @@ import io
 import base64
 import streamlit as st 
 
-def Make_new_Face(START=None):
+def Make_new_Face():
     a.get_event_loop_policy()#set_event_loop_policy(a.WindowsSelectorEventLoopPolicy)#get_event_loop()
     loop=a.new_event_loop()
     bytedata = loop.run_until_complete(get_online_person())
@@ -17,12 +17,14 @@ def Make_new_Face(START=None):
 def show():
     return st.image('face.png',caption="This is a computer generated image")
     
+def START(START=None):
+    Make_new_Face()
+    return show()
 # def facegen():
 #     img= loop.run_until_complete(get_online_person())#.close()
 
 #     img=Image.frombytes(img)
 #     img=Image.imshow(img)
 if __name__ == "__main__":
-    Make_new_Face()
-    show()
+    START()
 # facegen()
