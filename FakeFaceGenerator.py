35b99d53589686fe39c820bee32c8009db9f839e
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-09-04 17:56:35 +0530

    Update FakeFaceGenerator.py

--- FakeFaceGenerator.py
+++ FakeFaceGenerator.py
@@ -1,30 +1,32 @@
-def imports():
-    """The function get_online_person in library thispersondoesnotexist calls api and collects data from server in byte format. \n 
-    PIL library is used to do manipulation with image data. \n 
-    [Check Here](https://docs.python.org/3/library/asyncio.html?highlight=asyncio#module-asyncio). \n
-    The io module deals with various types of I/O in python. There are three main types of I/O: text I/O, binary I/O and raw I/O. \n 
-    Streamlit has been used to give our app a web interface."""
-    from thispersondoesnotexist import get_online_person
-    from  PIL import Image
-    import asyncio as a
-    import io
-    import streamlit as st 
+from thispersondoesnotexist import get_online_person
+from  PIL import Image
+import asyncio as a
+import io
+import streamlit as st 
 
-imports()
-
-def Make_new_Face():
+def Make_new_Face(name_of_image='face.png'):
+    """
+    PIL library is used to do manipulation with image data.
+    This function calls api and save the generated image.
+    The io module deals with various types of I/O in python. There are three main types of I/O: text I/O, binary I/O and raw I/O.
+    :param name_of_image : Set to face.png by defalt.
+    :return img.save : Our image data from byte format is converted into image format and saved with desired name"""
     a.get_event_loop_policy()#set_event_loop_policy(a.WindowsSelectorEventLoopPolicy)#get_event_loop()
     loop=a.new_event_loop()
     bytedata = loop.run_until_complete(get_online_person())
     loop.close()
     img=Image.open(io.BytesIO(bytedata))
-    return img.save("face.png")
+    return img.save(name_of_image)
     # return img.show()
 
-def showimage():
+def showimage(name_of_image='face.png'):
+    """This function will open the saved image by user with cmd mode
+    :param name_of_image : Set to face.png by default.
+    :return Image.open : Our image will be displayed with the default windows/linux image application"""
     return Image.open('face.png').show()
 
 def aface():
+    """This function is for cmd mode and will directly show an image on screen without saving."""
     a.get_event_loop_policy()
     loop=a.new_event_loop()
     bytedata = loop.run_until_complete(get_online_person())
@@ -33,12 +35,14 @@ def aface():
     return img.show()
 
 def show():
+    """This function will display our newly generated image on webpage."""
     return st.image('face.png')
     
 def START(START=None):
+    """In this function streamlit has been used to give our app a web interface."""
     st.title('Fake Face Generator')
-    st.header("This is a computer generated image")
-    if st.button("Generate Face"):
+    st.header('This is a computer generated image')
+    if st.button('Generate Face'):
         Make_new_Face()
         return show()
 
