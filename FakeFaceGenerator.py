a915843b8916df564fe8fe6e2c8f748105d1f2d0
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-09-04 17:17:01 +0530

    Update FakeFaceGenerator.py

--- FakeFaceGenerator.py
+++ FakeFaceGenerator.py
@@ -1,13 +1,16 @@
-"""The function get_online_person in library thispersondoesnotexist calls api and collects data from server in byte format""" 
-from thispersondoesnotexist import get_online_person
-"""PIL library is used to do manipulation with image data"""
-from  PIL import Image
-"""[Check Here](https://docs.python.org/3/library/asyncio.html?highlight=asyncio#module-asyncio)"""
-import asyncio as a
-"""The io module deals with various types of I/O in python. There are three main types of I/O: text I/O, binary I/O and raw I/O."""
-import io
-"""Streamlit has been used to give our app a web interface."""
-import streamlit as st 
+def imports():
+    """The function get_online_person in library thispersondoesnotexist calls api and collects data from server in byte format""" 
+    from thispersondoesnotexist import get_online_person
+    """PIL library is used to do manipulation with image data"""
+    from  PIL import Image
+    """[Check Here](https://docs.python.org/3/library/asyncio.html?highlight=asyncio#module-asyncio)"""
+    import asyncio as a
+    """The io module deals with various types of I/O in python. There are three main types of I/O: text I/O, binary I/O and raw I/O."""
+    import io
+    """Streamlit has been used to give our app a web interface."""
+    import streamlit as st 
+
+imports()
 
 def Make_new_Face():
     a.get_event_loop_policy()#set_event_loop_policy(a.WindowsSelectorEventLoopPolicy)#get_event_loop()
